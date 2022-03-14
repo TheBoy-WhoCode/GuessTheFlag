0df300ac9d46703061eb0bc637b5e41bd0905267
TheBoy-WhoCode <jiten.pateljp2001@gmail.com>
2022-03-14 10:09:59 +0530

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jiten Patel on 14/03/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
